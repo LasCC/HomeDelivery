f2b650c88986f740ca6c94100965ff5873ef3e88
LasCC <erazjean@gmail.com>
2020-06-04 02:38:23 +0200

    Update MapDev 🗺

--- src/components/MapDev.js
+++ src/components/MapDev.js
@@ -1,16 +1,8 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
-import MapGL, {
-  FlyToInterpolator,
-  GeolocateControl,
-  NavigationControl,
-  FullscreenControl,
-  Marker,
-} from "react-map-gl";
-import { GeoJsonLayer } from "deck.gl";
+import MapGL, { FlyToInterpolator, Marker } from "react-map-gl";
 import Pin from "./Pin";
 import MarkersCity from "../data/cities.json";
-import Geocoder from "react-map-gl-geocoder";
 import "mapbox-gl/dist/mapbox-gl.css";
 import "react-map-gl-geocoder/dist/mapbox-gl-geocoder.css";
 
@@ -18,27 +10,6 @@ import ControlPanel from "./ControlPanel";
 
 const MAPBOX_TOKEN =
   "pk.eyJ1IjoibWljaGFsbG93IiwiYSI6ImNrN3JraDE5aTBkcG0zbG91aWxzaGN1ZHYifQ.3y3s0EYnPlHXE5F_8-ySkg";
-
-const geolocateStyle = {
-  position: "absolute",
-  top: 0,
-  left: 0,
-  margin: 10,
-};
-
-const fullscreenControlStyle = {
-  position: "absolute",
-  top: 0,
-  left: 0,
-  padding: "10px",
-};
-
-const navStyle = {
-  position: "absolute",
-  top: 36,
-  left: 0,
-  padding: "10px",
-};
 
 const cardsStyle = {
   position: "relative",
@@ -49,7 +20,6 @@ const cardsStyle = {
 };
 
 export default class App extends Component {
-  mapRef = React.createRef();
   state = {
     viewport: {
       latitude: 48.84978,
@@ -57,7 +27,6 @@ export default class App extends Component {
       zoom: 12,
       pitch: 60,
     },
-    searchResultLayer: null,
   };
   handleViewportChange = (viewport) => {
     this.setState({
@@ -74,26 +43,10 @@ export default class App extends Component {
     });
   };
 
-  handleOnResult = (event) => {
-    this.setState({
-      searchResultLayer: new GeoJsonLayer({
-        id: "search-result",
-        data: event.result.geometry,
-        getFillColor: [255, 0, 0, 128],
-        getRadius: 1000,
-        pointRadiusMinPixels: 10,
-        pointRadiusMaxPixels: 10,
-      }),
-    });
-  };
-  _onViewportChange = (viewport) => this.setState({ viewport });
+  onViewportChange = (viewport) => this.setState({ viewport });
 
-  _onClickMarker = (city) => {
-    this.setState({ popupInfo: city });
-  };
-
-  _goToViewport = ({ longitude, latitude }) => {
-    this._onViewportChange({
+  goToViewport = ({ longitude, latitude }) => {
+    this.onViewportChange({
       longitude,
       latitude,
       zoom: 16,
@@ -103,32 +56,18 @@ export default class App extends Component {
   };
 
   render() {
-    const { viewport, settings, searchResultLayer } = this.state;
+    const { viewport } = this.state;
 
     return (
       <MapGL
         {...viewport}
-        {...settings}
-        ref={this.mapRef}
         width='100%'
         height='100%'
         mapStyle='mapbox://styles/michallow/ck86fkoji05jh1ipb0l9rlfy7'
         onViewportChange={this.handleViewportChange}
         dragRotate={true}
         mapboxApiAccessToken={MAPBOX_TOKEN}
-        layers={[searchResultLayer]}
       >
-        <GeolocateControl
-          style={geolocateStyle}
-          positionOptions={{ enableHighAccuracy: true }}
-          trackUserLocation={true}
-        />
-        <div style={fullscreenControlStyle}>
-          <FullscreenControl />
-        </div>
-        <div style={navStyle}>
-          <NavigationControl />
-        </div>
         {/* Pins */}
         {MarkersCity.map((command) => {
           return (
@@ -137,20 +76,11 @@ export default class App extends Component {
             </Marker>
           );
         })}
-        <Geocoder
-          mapRef={this.mapRef}
-          onResult={this.handleOnResult}
-          onViewportChange={this._goToViewport}
-          mapboxApiAccessToken={MAPBOX_TOKEN}
-          position='top-right'
-          marker={{ color: "red" }}
-          placeholder='Rechercher une adresse'
-        />
         <div style={cardsStyle}>
           {/* Cards on the map */}
           <ControlPanel
             containerComponent={this.props.containerComponent}
-            onViewportChange={this._goToViewport}
+            onViewportChange={this.goToViewport}
           />
         </div>
       </MapGL>
